fcac8fe1abb7dd5c66afc74d9bffa9020e7c04db
Adarsh rajput <adarshraj7457@gmail.com>
2026-09-11 14:51:04 +0530

    Question7 Assignment04

---
+++ Question7.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program7
+{
+    delegate void MyDelegate();
+
+    static void Method1()
+    {
+        Console.WriteLine("Method1 is executed");
+    }
+
+    static void Method2()
+    {
+        Console.WriteLine("Method2 is executed");
+    }
+
+    static void Method3()
+    {
+        Console.WriteLine("Method3 is executed");
+    }
+
+    static void Main()
+    {
+        MyDelegate d = Method1;
+        d += Method2;
+        d += Method3;
+
+        Console.WriteLine("Methods stored in delegate:");
+
+        foreach (Delegate method in d.GetInvocationList())
+        {
+            Console.WriteLine("Method Name: " + method.Method.Name);
+            method.DynamicInvoke();
+        }
+
+        Console.ReadLine();
+    }
+}
