54842772ae26e9afed745d137c1dd97a50b308a0
Adarsh rajput <adarshraj7457@gmail.com>
2026-09-11 14:36:14 +0530

    Add files via upload
    Use GetInvocationList() to execute
    each method separately and display the return value of every salary component.

---
+++ Question6.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class SalaryCalculator
+{
+    public static double BasicSalary(double basic)
+    {
+        return basic;
+    }
+    public static double HRA(double basic)
+    {
+        return basic * 0.20;
+    }
+
+    public static double DA(double basic)
+    {
+        return basic * 0.10;
+    }
+
+    public static double Bonus(double basic)
+    {
+        return basic * 0.15;
+    }
+}
+
+class Program
+{
+   
+    public delegate double SalaryDelegate(double basic);
+
+    static void Main(string[] args)
+    {
+        Console.Write("Enter Basic Salary: ");
+        double basic = Convert.ToDouble(Console.ReadLine());
+
+        SalaryDelegate salary = SalaryCalculator.BasicSalary;
+        salary += SalaryCalculator.HRA;
+        salary += SalaryCalculator.DA;
+        salary += SalaryCalculator.Bonus;
+
+        Console.WriteLine("\nSalary Components:");
+
+        foreach (SalaryDelegate method in salary.GetInvocationList())
+        {
+            double result = method(basic);
+
+            Console.WriteLine(method.Method.Name + " = " + result);
+        }
+
+        Console.ReadLine();
+    }
+}
