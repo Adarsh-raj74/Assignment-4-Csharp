3d62ceb460d8fc7e21ea949d1a678a9a67458612
Adarsh rajput <adarshraj7457@gmail.com>
2026-09-15 13:53:11 +0530

    Unicast & multicast display massage

---
+++ Question8.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void DisplayMessage()
+    {
+        Console.WriteLine("Hello from DisplayMessage()");
+    }
+    static void ShowMessage()
+    {
+        Console.WriteLine("Hello from ShowMessage()");
+    }
+
+    static void Main()
+    {
+        Action action1;
+
+        action1 = DisplayMessage;
+
+        Console.WriteLine("Unicast Action Delegate:");
+        action1();
+
+        Action action2;
+
+        action2 = DisplayMessage;
+        action2 += ShowMessage;
+
+        Console.WriteLine("\nMulticast Action Delegate:");
+        action2();
+    }
+}
